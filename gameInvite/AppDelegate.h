aca39e141f99cc15b1573f2337e3bbbc987daf50
sevi <sevi@sevis-macbook-pro.local>
2015-08-14 16:28:01 +0800

    Initial Commit

---
+++ gameInvite/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gameInvite
+//
+//  Created by sevi on 8/14/15.
+//  Copyright © 2015 sevi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gameInvite/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gameInvite
+//
+//  Created by sevi on 8/14/15.
+//  Copyright © 2015 sevi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
